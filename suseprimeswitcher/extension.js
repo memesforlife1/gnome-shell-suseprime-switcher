7dec69dbb1b2387cfc6993025a8ecec8feba8b18
memesforlife1 <68858960+memesforlife1@users.noreply.github.com>
2021-08-15 09:24:21 -0400

    Add files via upload

--- suseprimeswitcher/extension.js
+++ suseprimeswitcher/extension.js
@@ -22,64 +22,58 @@ const SusePrimeSwitcher = new Lang.Class({
 
 		this.parent(1, 'SUSEPrimeSwitcher', false);
 
-		let nvidiaicon =	Gio.icon_new_for_string(Me.path + '/icons/suseprime-nvidia-symbolic.svg');
-		let intelicon =	Gio.icon_new_for_string(Me.path + '/icons/suseprime-intel-symbolic.svg');
-		let primeicon =	Gio.icon_new_for_string(Me.path + '/icons/suseprime-symbolic.svg');
-		let currentType;
-		if (GLib.file_test('/etc/prime/current_type', GLib.FileTest.EXISTS)){
-			currentType = String(GLib.file_get_contents('/etc/prime/current_type')[1]).trim();
-		}
-		else {
-      currentType = '';
-		}
+		let nvidiaicon =  Gio.icon_new_for_string(Me.path + '/icons/suseprime-nvidia-symbolic.svg');
+		let intelicon =  Gio.icon_new_for_string(Me.path + '/icons/suseprime-intel-symbolic.svg');
+		let primeicon =  Gio.icon_new_for_string(Me.path + '/icons/suseprime-symbolic.svg');
+		let currentType = String(GLib.file_get_contents('/etc/prime/current_type')[1]).trim();
 		let box = new St.BoxLayout();
 		let gicon;
 
 		if (currentType.indexOf('intel') != -1) {
-			gicon = intelicon;
-		}
-		else if (currentType.indexOf('nvidia') != -1) {
-			gicon = nvidiaicon;
-		}
-		else {
-			gicon = primeicon;
-		}
+      gicon = intelicon;
+    }
+    else if (currentType.indexOf('nvidia') != -1) {
+      gicon = nvidiaicon;
+    }
+    else {
+      gicon = primeicon;
+    }
 
-		let icon =	new St.Icon();
+		let icon =  new St.Icon({ style_class: 'system-status-icon' });
 		icon.gicon = gicon;
-		icon.style_class = 'system-status-icon';
+
 		box.add(icon);
+
 		this.actor.add_child(box);
 
-		let summaryMenuItem = new PopupMenu.PopupMenuItem(_(`Currently using ${currentType}`), { style_class: "no-background" });
-
-		if ( currentType ) {
-			this.menu.addMenuItem(summaryMenuItem);
-		}
-		// In case the state is undefined, show both
+		let summaryMenuItem = new PopupMenu.PopupMenuItem(_(`Currently using ${currentType}`), {
+      style_class: "no-background"
+    });
 		let nvidiaMenuItem = new PopupMenu.PopupImageMenuItem(_('Switch to Nvidia'), nvidiaicon);
-		if ( currentType.indexOf('nvidia') == -1 ) {
-			this.menu.addMenuItem(nvidiaMenuItem);
-		}
 		let intelMenuItem = new PopupMenu.PopupImageMenuItem(_('Switch to Intel'), intelicon);
-		if ( currentType.indexOf('intel') == -1 ) {
-			this.menu.addMenuItem(intelMenuItem);
-		}
 
-		let infoMenuItem = new PopupMenu.PopupMenuItem(_('Restart your session after switching!'), { style_class: "menu-item-small" });
-		this.menu.addMenuItem(infoMenuItem);
+    if (currentType) {
+      this.menu.addMenuItem(summaryMenuItem);
+    }
+    // In case the state is undefined, show both
+		if ( currentType.indexOf('nvidia') == -1 ) {
+		  this.menu.addMenuItem(nvidiaMenuItem);
+		}
+		if ( currentType.indexOf('intel') == -1 ) {
+		  this.menu.addMenuItem(intelMenuItem);
+		}
+		let infoMenuItem = new PopupMenu.PopupMenuItem(_('Restart your session after switching!'), {
+      style_class: "menu-item-small"
+    });
+    this.menu.addMenuItem(infoMenuItem);
 
 		nvidiaMenuItem.connect('activate', Lang.bind(this, function(){
-			Util.trySpawnCommandLine(Me.path + '/pkexec_nvidia');
-			Main.notify(_('Restart or log out and back in to finish the switch'));
-			icon.style_class = 'system-status-icon screencast-indicator';
-			icon.gicon = nvidiaicon;
-		}));
+		  Util.trySpawnCommandLine(Me.path + '/pkexec_nvidia');
+		  Main.notify(_('Restart or log out and back in to finish the switch'));
+	  }));
 		intelMenuItem.connect('activate', Lang.bind(this, function(){
-			Util.trySpawnCommandLine(Me.path + '/pkexec_intel');
-			Main.notify(_('Restart or log out and back in to finish the switch'));
-			icon.style_class = 'system-status-icon screencast-indicator';
-			icon.gicon = intelicon;
+		  Util.trySpawnCommandLine(Me.path + '/pkexec_intel');
+		  Main.notify(_('Restart or log out and back in to finish the switch'));
 		}));
 
 	},
@@ -91,10 +85,10 @@ const SusePrimeSwitcher = new Lang.Class({
 function init() {}
 
 function enable() {
-		button = new SusePrimeSwitcher;
-		Main.panel.addToStatusArea('SUSEPrimeSwitcher', button, 0, 'right');
+    button = new SusePrimeSwitcher;
+    Main.panel.addToStatusArea('SUSEPrimeSwitcher', button, 0, 'right');
 }
 
 function disable() {
-		button.destroy();
+    button.destroy();
 }
